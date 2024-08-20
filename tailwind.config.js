a553189518c4f20796448a9bdcd2407c8d8738f5
Shiva1796 <shiva96chawla@gmail.com>
2024-08-20 17:59:52 -0400

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Josefin: ["Josefin Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
