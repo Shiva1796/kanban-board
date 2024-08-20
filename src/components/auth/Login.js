ff93fedf62f8e9966921e6a2bd86ee4c1e9dd368
Shiva1796 <shiva96chawla@gmail.com>
2024-08-20 19:19:44 -0400

    Added protected routes and auth context

---
+++ src/components/auth/Login.js
@@ -0,0 +1,48 @@
+// src/components/body/LoginPage.js
+import React, { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await login(email, password);
+      navigate("/");
+    } catch (err) {
+      setError("Failed to log in");
+    }
+  };
+
+  return (
+    <div className="">
+      <h2>Login</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { CardProvider } from "./components/context/CardContext";
+import { AuthProvider } from "./components/context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CardProvider>
-      <App />
-    </CardProvider>
+    <AuthProvider>
+      <CardProvider>
+        <App />
+      </CardProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
 
